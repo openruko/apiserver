43bb531182f608eaf41e76b748dc03ac1f89b0bc
Thomas Buckley-Houston <tom@tombh.co.uk>
2013-01-05 00:30:14 +0000

    Refactor handleGitCommand into deploymethod

--- apiserver/api/deployer.js
+++ apiserver/api/deployer.js
@@ -1,14 +1,68 @@
+var db = require('../apidb');
+var dbfacade= require('../dbfacade')(db);
+var async = require('async');
+
 module.exports = {
+
   destroyApp: {
     routePath : '/apps/:appName/deploy',
     payloadSource: 'query',
     method: 'PUT',
     okayCode: 200,
+
+    // Presence of 'handler' overrides attempt to call DB function with same name as route key
     handler: function(cb) {
-      var payload = this.requestPayload;
-      this.responsePayload = {"appName": payload.appName, "github_url": payload.app.github_url};
-      cb();
-    },
-    errorCode: 404
+      var self = this;
+
+      // This isn't needed as we're doing `git pull` now
+      this.requestPayload.command = '/usr/bin/git-recieve-pack';
+
+      // var appName = requestPayload.appName;
+      // var githubUrl = requestPayload.app.github_url;
+
+      // Get a job to build the new slug
+      dbfacade.exec('handleGitCommand', this.requestPayload, function(dbError, dbResult) {
+        if(dbError) return cb(dbError);
+
+        var timesQueried = 0;
+        var jobId = dbResult.rows[0].id;
+        var result;
+
+        // Get connection details to the build dyno
+        async.whilst(function() {
+          return (!result) && timesQueried < 20;
+        }, function(callback) {
+          dbfacade.exec('getJob', { jobId: jobId }, function(err, dbResult) {
+            timesQueried++;
+            if(err) return callback(err);
+
+            var job = dbResult.rows[0];
+            if(job.distributed_to) {
+              result = {
+                host: job.distributed_to,
+                dyno_id: job.dyno_id,
+                rez_id: job.rez_id
+              };
+            }
+            if(result) {
+              callback();
+            } else {
+              setTimeout(callback, 500);
+            }
+          });
+        }, function(err) {
+          if(err || !result) {
+            console.log('Unable to assign job');
+            return cb({ error: 'job not assigned' });
+          }
+          self.responsePayload = result;
+
+          // Connect to the dyno, but how!?
+
+          cb();
+        });
+      });
+
+    }
   }
 };
